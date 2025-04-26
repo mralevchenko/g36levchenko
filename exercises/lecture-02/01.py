95e0adc73e68558307e507cc05be67ec0537dfa0
mralevchenko <mr.a.levchenko@gmail.com>
2025-04-27 00:54:47 +0300

    Update 01.py

--- exercises/lecture-02/01.py
+++ exercises/lecture-02/01.py
@@ -1,1 +1,21 @@
+str1 = input("Enter a string: ")
 
+first = str1[0]
+middle = str1[len(str1) // 2]
+last = str1[-1]
+new_string = first + middle + last
+print("New string:", new_string)
+
+
+length = len(str1)
+if length % 2 == 0:
+    first_half = str1[:length // 2]
+    second_half = str1[length // 2:]
+else:
+    first_half = str1[:length // 2]
+    second_half = str1[length // 2 + 1:]
+
+if first_half == second_half:
+    print("The entered string is symmetrical")
+else:
+    print("The entered string is not symmetrical")
