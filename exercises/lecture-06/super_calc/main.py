6fdeda8094006f3d56fad6d2df341effe79c76fe
mralevchenko <mr.a.levchenko@gmail.com>
2025-04-28 00:00:11 +0300

    Update main.py

--- exercises/lecture-06/super_calc/main.py
+++ exercises/lecture-06/super_calc/main.py
@@ -1,1 +1,17 @@
+from menu import menu
+from calculator import run_calculator
+from helpers import calc_help
 
+def main():
+    while True:
+        choice = menu()
+        if choice == 'q':
+            print('Thank you for using Super Calc!')
+            break
+        elif choice == 'h':
+            calc_help()
+        elif choice == 'c':
+            run_calculator()
+
+if __name__ == '__main__':
+    main()
