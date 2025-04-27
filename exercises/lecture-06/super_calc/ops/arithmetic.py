5d67b32124b40e7460a59900a19cbea2e49937c7
mralevchenko <mr.a.levchenko@gmail.com>
2025-04-28 00:01:40 +0300

    Update arithmetic.py

--- exercises/lecture-06/super_calc/ops/arithmetic.py
+++ exercises/lecture-06/super_calc/ops/arithmetic.py
@@ -1,1 +1,17 @@
+def add(x, y): return x + y
+def subtract(x, y): return x - y
+def multiply(x, y): return x * y
+def divide(x, y): return x / y
+def idivide(x, y): return x // y
+def modulo(x, y): return x % y
+def exponent(x, y): return x ** y
 
+ops = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+    '//': idivide,
+    '%': modulo,
+    '**': exponent
+}
