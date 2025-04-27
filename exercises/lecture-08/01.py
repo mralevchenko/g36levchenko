1c8f07f5983c2bca1356c3959895245882d224d0
mralevchenko <mr.a.levchenko@gmail.com>
2025-04-27 22:46:55 +0300

    Update 01.py

--- exercises/lecture-08/01.py
+++ exercises/lecture-08/01.py
@@ -1,1 +1,16 @@
+# Генераторна функція для нескінченної послідовності Фібоначчі
+def fib():
+    a, b = 0, 1
+    while True:
+        yield b
+        a, b = b, a + b
 
+# Створення списку до значення 610
+fib_list = []
+
+for n in fib():
+    if n > 610:
+        break
+    fib_list.append(n)
+
+print(fib_list)
