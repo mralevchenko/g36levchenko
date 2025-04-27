122ed3dddae09ab699634a0b530c5d306838fbcb
mralevchenko <mr.a.levchenko@gmail.com>
2025-04-27 23:59:29 +0300

    Update calculator.py

--- exercises/lecture-06/super_calc/calculator.py
+++ exercises/lecture-06/super_calc/calculator.py
@@ -1,1 +1,15 @@
+from helpers import extract, calc_help, result
 
+def run_calculator():
+    entry = input("Enter x operator y (e.g., 2 + 2): ")
+    a, b, operator = extract(entry)
+
+    if not operator:
+        calc_help("Invalid format or unsupported operator.")
+        return
+
+    res, err = result(a, b, operator)
+    if err:
+        print(err)
+    else:
+        print(f"{a} {operator} {b} = {res}")
