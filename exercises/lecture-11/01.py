e8c607ba0fde899bde3fb6eceb542876642f562e
mralevchenko <mr.a.levchenko@gmail.com>
2025-04-29 00:20:12 +0300

    Update 01.py

--- exercises/lecture-11/01.py
+++ exercises/lecture-11/01.py
@@ -1,1 +1,20 @@
+import time
+
+# Декоратор, що затримує виконання функції на задану кількість секунд
+def delay(seconds):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            time.sleep(seconds)  # Затримка перед виконанням функції
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+# Функція, яка буде декорована
+@delay(5)
+def it_may_be(name=''):
+    return f"{name} ask: It's nearly Luncheon Time? \n"
+
+# Виклик функції через цикл
+for t in range(12):
+    print(it_may_be(name='Winnie-the-Pooh'))
 
