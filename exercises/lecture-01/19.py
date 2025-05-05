e0de99c3aa30b0e9bf87b136827548364ed673bb
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-06 00:07:09 +0300

    Update 19.py

--- exercises/lecture-01/19.py
+++ exercises/lecture-01/19.py
@@ -1,1 +1,12 @@
+# Початкова сума вкладу
+deposit = 1000
+
+# Відсоток нарахування
+interest_rate = 0.08
+
+# Обчислюємо суму на рахунку через рік
+new_balance = deposit * (1 + interest_rate)
+
+# Виводимо результат
+print(new_balance)
 
