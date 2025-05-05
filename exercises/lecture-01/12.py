f8384873779729771a7c0457563b47c5830c4289
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-06 00:03:09 +0300

    Update 12.py

--- exercises/lecture-01/12.py
+++ exercises/lecture-01/12.py
@@ -1,1 +1,14 @@
+# Читання двох чисел
+a = int(input())
+b = int(input())
+
+# Виконання арифметичних операцій
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+exponentiation = a ** b
+
+# Виведення результатів через символ "&"
+print(f"{addition}&{subtraction}&{multiplication}&{division}&{exponentiation}")
 
