d33b36cd0dfb1d6bfd184e12a3635179640550bd
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-05 23:52:44 +0300

    Update 04.py

--- exercises/lecture-01/04.py
+++ exercises/lecture-01/04.py
@@ -1,1 +1,13 @@
+# Константи для переведення
+FOOT_TO_CM = 30.48
+INCH_TO_CM = 2.54
 
+# Введення користувача
+feet = int(input("Feet: "))
+inches = int(input("Inches: "))
+
+# Обчислення
+height_cm = feet * FOOT_TO_CM + inches * INCH_TO_CM
+
+# Виведення результату
+print(f"Your height is: {height_cm:.1f} cm.")
