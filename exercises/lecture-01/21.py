f82cf5d6b0bca73d1c413138a0773024e3f54430
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-06 00:08:14 +0300

    Update 21.py

--- exercises/lecture-01/21.py
+++ exercises/lecture-01/21.py
@@ -1,1 +1,13 @@
+def convert_to_time(n):
+    hours = n // 60  # Кількість годин
+    minutes = n % 60  # Кількість хвилин
+    return hours, minutes
+
+# Вхідні дані
+times = [150, 25, 440]
+
+# Обробка і виведення результатів
+for time in times:
+    hours, minutes = convert_to_time(time)
+    print(hours, minutes)
 
