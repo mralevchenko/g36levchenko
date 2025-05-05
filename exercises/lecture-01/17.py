66007d21df7ae197040d07775b079e6825b9f5e7
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-06 00:06:04 +0300

    Update 17.py

--- exercises/lecture-01/17.py
+++ exercises/lecture-01/17.py
@@ -1,1 +1,10 @@
+# Зчитуємо платню і податок
+salary = float(input())  # Платня (грн)
+tax = float(input())  # Податок у відсотках
+
+# Обчислюємо чисту зарплату
+net_salary = salary * (1 - tax / 100)
+
+# Виводимо результат
+print(net_salary)
 
