bc362286e8dead075dad39855b17f252a3464276
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-06 00:02:25 +0300

    Update 11.py

--- exercises/lecture-01/11.py
+++ exercises/lecture-01/11.py
@@ -1,1 +1,18 @@
+# Читання двох змінних
+a = int(input())
+b = int(input())
+
+# Виконання арифметичних операцій
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+exponentiation = a ** b
+
+# Виведення результатів
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(exponentiation)
 
