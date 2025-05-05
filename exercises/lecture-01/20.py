cb890132ca723cca1195b2fc7c74c05ef9289d45
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-06 00:07:40 +0300

    Update 20.py

--- exercises/lecture-01/20.py
+++ exercises/lecture-01/20.py
@@ -1,1 +1,19 @@
+def convert_seconds(seconds):
+    # Обчислюємо кількість днів, годин, хвилин і секунд
+    days = seconds // 86400
+    seconds %= 86400
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    
+    # Повертаємо результат у вигляді рядка
+    return f"{days} day(s), {hours} hour(s), {minutes} minute(s), {seconds} second(s)."
+
+# Вхідні дані
+times = [6785, 456789, 86401]
+
+# Обробка кожного значення і виведення результату
+for time in times:
+    print(convert_seconds(time))
 
