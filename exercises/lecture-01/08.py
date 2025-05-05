6603c63d093e020781f2c747e9cc459399d09fb3
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-05 23:56:46 +0300

    Update 08.py

--- exercises/lecture-01/08.py
+++ exercises/lecture-01/08.py
@@ -1,1 +1,12 @@
+# Зчитуємо введені значення
+days = int(input("Days: "))
+hours = int(input("Hours: "))
+minutes = int(input("Minutes: "))
+seconds = int(input("Seconds: "))
+
+# Перетворюємо все в секунди
+total_seconds = days * 86400 + hours * 3600 + minutes * 60 + seconds
+
+# Виводимо результат
+print(f"The amounts of seconds: {total_seconds}")
 
