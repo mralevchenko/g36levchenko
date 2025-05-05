14532afe37b8bffe949716304e09676022398f67
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-06 00:04:19 +0300

    Update 14.py

--- exercises/lecture-01/14.py
+++ exercises/lecture-01/14.py
@@ -1,1 +1,12 @@
+# Читання введеного значення
+user_input = input()
+
+# Перетворення у дійсне число
+float_value = float(user_input)
+
+# Перетворення в ціле число
+int_value = int(float_value)
+
+# Виведення результату
+print(int_value)
 
