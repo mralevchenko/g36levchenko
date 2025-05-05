bd3e43c1b9fc396649c64bf00e0abcc35e1e8871
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-05 23:57:25 +0300

    Update 09.py

--- exercises/lecture-01/09.py
+++ exercises/lecture-01/09.py
@@ -1,1 +1,11 @@
+# Вводимо значення
+f = float(input("Enter the desired future value: "))  # майбутня сума
+r = float(input("Enter the annual interest rate: "))  # річна відсоткова ставка
+n = int(input("Enter the number of years the money will grow: "))  # кількість років
+
+# Обчислюємо поточну суму, яку потрібно внести
+p = f / ((1 + r) ** n)
+
+# Виводимо результат
+print(f"You will need to deposit this amount: {p:.2f}.")
 
