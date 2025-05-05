cdf8761fb4e08563fed48a5ac982833bc144cfb1
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-06 00:04:55 +0300

    Update 15.py

--- exercises/lecture-01/15.py
+++ exercises/lecture-01/15.py
@@ -1,1 +1,13 @@
+# Зчитуємо швидкість з клавіатури
+speed = float(input())
+
+# Обчислюємо відстань для кожного часу
+distance_6_hours = speed * 6
+distance_10_hours = speed * 10
+distance_15_hours = speed * 15
+
+# Виводимо результат
+print(distance_6_hours)
+print(distance_10_hours)
+print(distance_15_hours)
 
