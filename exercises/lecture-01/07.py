1c7c1db5e09f9f988a6dd1d4f7d26b1719cf60a8
mralevchenko <mr.a.levchenko@gmail.com>
2025-05-05 23:56:17 +0300

    Update 07.py

--- exercises/lecture-01/07.py
+++ exercises/lecture-01/07.py
@@ -1,1 +1,11 @@
+# Зчитуємо три цифри
+digit1 = int(input())
+digit2 = int(input())
+digit3 = int(input())
+
+# Обчислюємо суму
+total = digit1 + digit2 + digit3
+
+# Виводимо результат
+print(total)
 
